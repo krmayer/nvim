5a8d5311a2eb0f986c97f0c68e45a66f58833cf3
Kyle Mayer <mayer.kyler@gmail.com>
2026-01-18 23:43:36 -0800

    Kickstart stripped down 2

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.o.number = true
+vim.o.relativenumber = true
 vim.o.mouse = 'a'
 vim.o.clipboard = 'unnamedplus'
 vim.o.undofile = true
@@ -17,6 +18,25 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function() vim.hl.on_yank() end,
+})
+
+vim.diagnostic.config {
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  signs = {},
+  virtual_text = { source = 'if_many', spacing = 2 },
+}
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', 'https://github.com/folke/lazy.nvim.git', lazypath }
@@ -29,7 +49,7 @@ require('lazy').setup({
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs',
     opts = {
-      ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'bash' },
+      ensure_installed = { 'c', 'cpp', 'lua', 'python', 'vim', 'vimdoc', 'bash' },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
@@ -43,6 +63,7 @@ require('lazy').setup({
       { '<leader>f', '<cmd>Telescope find_files<cr>' },
       { '<leader>g', '<cmd>Telescope live_grep<cr>' },
       { '<leader>b', '<cmd>Telescope buffers<cr>' },
+      { '<leader>/', '<cmd>Telescope current_buffer_fuzzy_find<cr>' },
     },
   },
 
@@ -51,22 +72,74 @@ require('lazy').setup({
     dependencies = {
       { 'mason-org/mason.nvim', opts = {} },
       'mason-org/mason-lspconfig.nvim',
+      'saghen/blink.cmp',
     },
     config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+      local servers = {
+        clangd = {},
+        lua_ls = {
+          settings = {
+            Lua = { completion = { callSnippet = 'Replace' } },
+          },
+        },
+        ruff = {},
+      }
+
+      require('mason-lspconfig').setup {
+        ensure_installed = vim.tbl_keys(servers),
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            require('lspconfig')[server_name].setup(server)
+          end,
+        },
+      }
+
       vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+        callback = function(event)
+          local map = function(keys, func)
+            vim.keymap.set('n', keys, func, { buffer = event.buf })
           end
+          map('grd', vim.lsp.buf.definition)
+          map('grr', vim.lsp.buf.references)
+          map('grn', vim.lsp.buf.rename)
+          map('gra', vim.lsp.buf.code_action)
+          map('K', vim.lsp.buf.hover)
         end,
       })
-
-      require('mason-lspconfig').setup { ensure_installed = { 'clangd', 'lua_ls' } }
-      require('lspconfig').clangd.setup {}
-      require('lspconfig').lua_ls.setup {}
     end,
   },
+
+  {
+    'saghen/blink.cmp',
+    version = '1.*',
+    opts = {
+      keymap = { preset = 'default' },
+      completion = { documentation = { auto_show = true } },
+      sources = { default = { 'lsp', 'path', 'buffer' } },
+      signature = { enabled = true },
+      fuzzy = { implementation = 'lua' },
+    },
+  },
+
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    opts = {
+      format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'ruff_format' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
+      },
+    },
+  },
+
+  { 'numToStr/Comment.nvim', opts = {} },
 
   {
     'folke/tokyonight.nvim',
